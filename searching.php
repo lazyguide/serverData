a64e0188537c9b36881269369e4c638c5d7ddf26
tsaiyichen <easonjessiejj@gmail.com>
2024-01-24 15:19:52 +0800

    2024/01/24 searching.php dormitory

--- searching.php
+++ searching.php
@@ -9,4 +9,8 @@ $link = @mysqli_connect("localhost", "root", '12346578', 'lazyguide');
 $buildingSQL = 'SELECT * FROM building WHERE buildingID LIKE "%'.$keyword.'%" OR buildingName LIKE "%'.$keyword.'%" OR buildingDescription LIKE "%'.$keyword.'%"';
 
 $buildingResult = mysqli_query($link, $buildingSQL);
+
+$dormitorySQL = 'SELECT * FROM dormitory WHERE dormitoryID LIKE "%'.$keyword.'%" OR dormitoryName LIKE "%'.$keyword.'%" OR dormitoryDescription LIKE "%'.$keyword.'%"';
+
+$dormitoryResult = mysqli_query($link, $dormitorySQL);
 ?>
