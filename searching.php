a4a9ef87b03c7ca5bb0c03ea43135e4e99940be6
tsaiyichen <easonjessiejj@gmail.com>
2024-01-25 15:50:37 +0800

    2024.01.25 searching.php facility library

--- searching.php
+++ searching.php
@@ -7,10 +7,16 @@ include "config,php";
 $link = @mysqli_connect("localhost", "root", '12346578', 'lazyguide');
 
 $buildingSQL = 'SELECT * FROM building WHERE buildingID LIKE "%'.$keyword.'%" OR buildingName LIKE "%'.$keyword.'%" OR buildingDescription LIKE "%'.$keyword.'%"';
-
 $buildingResult = mysqli_query($link, $buildingSQL);
 
-$dormitorySQL = 'SELECT * FROM dormitory WHERE dormitoryID LIKE "%'.$keyword.'%" OR dormitoryName LIKE "%'.$keyword.'%" OR dormitoryDescription LIKE "%'.$keyword.'%"';
-
+$dormitorySQL = 'SELECT * FROM dormitory WHERE dormitoryName LIKE "%'.$keyword.'%" OR dormitoryDescription LIKE "%'.$keyword.'%"';
 $dormitoryResult = mysqli_query($link, $dormitorySQL);
+
+$facilitySQL = 'SELECT * FROM dormitory WHERE dormitoryName LIKE "%'.$keyword.'%" OR dormitoryDescription LIKE "%'.$keyword.'%"';
+$facilityResult = mysqli_query($link, $facilitySQL);
+
+$librarySQL = 'SELECT * FROM library WHERE libraryID LIKE "%'.$keyword.'%" OR libraryName LIKE "%'.$keyword.'%" OR libraryDescription LIKE "%'.$keyword.'%"';
+$libraryResult = mysqli_query($link, $librarySQL);
+
+
 ?>
