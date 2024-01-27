a85ecb4a01e248ee73b2e2a153686e3383b37fc9
tsaiyichen <easonjessiejj@gmail.com>
2024-01-27 21:38:00 +0800

    2024/01/27 floorTest.php buildingID

--- floorTest.php
+++ floorTest.php
@@ -1,9 +1,9 @@
 <?php
-$building = "LM";
+$buildingID = "LM";
 $floor = 1;
 
 $link = @mysqli_connect('localhost', 'root', '12345678', 'lazyguide');
-$sql = "SELECT * FROM room WHERE building = '$building' AND floor = $floor";
+$sql = "SELECT * FROM room WHERE buildingID = '$buildingID' AND FLOOR = $floor";
 $result = mysqli_query($link, $sql);
 
 $array = [];
