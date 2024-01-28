70ce6a96419f164cb0c2da06fdd6788943c67550
tsaiyichen <easonjessiejj@gmail.com>
2024-01-28 21:49:47 +0800

    2024/01/27 searching_building.php testRow

--- search_building.php
+++ search_building.php
@@ -6,7 +6,7 @@ $sql = "SELECT * FROM building WHERE BUILDINGID = '$building'";
 
 $result = mysqli_query($link, $sql);
 
-$row = mysqli_fetch_assoc($result);
+$row = mysqli_fetch_row($result);
 
 echo json_encode($row);
 
