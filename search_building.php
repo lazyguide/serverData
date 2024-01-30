1d99dab37ced3d0bfbbe37e6dfb0bf164d50231a
tsaiyichen <easonjessiejj@gmail.com>
2024-01-30 17:36:10 +0800

    2024/01/30 search_building.php delete echo

--- search_building.php
+++ search_building.php
@@ -3,7 +3,6 @@ $keyword = $_POST['keyword'];
 $link = @mysqli_connect("localhost", "root", "12345678", "lazyguide");
 
 $buildingSQL = "SELECT * FROM building WHERE BUILDINGID LIKE '%".$keyword."%' OR BUILDINGNAME LIKE '%".$keyword."%' OR BUILDINGDESCRIPTION LIKE '%".$keyword."%'";
-echo $buildingSQL;
 $buildingResult = mysqli_query($link, $buildingSQL);
 
 while($row = mysqli_fetch_assoc($buildingResult)) {
