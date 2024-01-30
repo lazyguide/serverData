c2686776fa07258f3bac2f2abbe6994777f8180c
tsaiyichen <easonjessiejj@gmail.com>
2024-01-30 16:29:23 +0800

    2024/01/30 search_building.php

--- searching.php
+++ searching.php
@@ -1,9 +1,6 @@
 <?php
 
 $keyword = $_POST['keyword'];
-
-include "config,php";
-
 $link = @mysqli_connect("localhost", "root", '12346578', 'lazyguide');
 
 $buildingSQL = 'SELECT * FROM building WHERE buildingID LIKE "%'.$keyword.'%" OR buildingName LIKE "%'.$keyword.'%" OR buildingDescription LIKE "%'.$keyword.'%"';
