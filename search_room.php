6b0796ccd96b645e0d8c180cf946eb1564bea6ce
tsaiyichen <easonjessiejj@gmail.com>
2024-02-01 13:23:21 +0800

    2024/02/01 search_room.php

---
+++ search_room.php
@@ -0,0 +1,21 @@
+<?php
+$keyword = $_POST['keyword'];
+$link = @mysqli_connect("localhost", "root", "12345678", "lazyguide");
+
+$roomSQL = "SELECT * FROM room WHERE BUILDINGID LIKE '%".$keyword."%' OR BUILDINGNAME LIKE '%".$keyword."%' OR BUILDINGDESCRIPTION LIKE '%".$keyword."%'";
+$roomResult = mysqli_query($link, $roomSQL);
+
+while($roomRow = mysqli_fetch_assoc($roomResult)) {
+    echo json_encode($roomRow);
+}
+
+if(strlen($keyword)){
+    $building = substr($keyword,0,2);
+    $room = substr($keyword, 2);
+    $SQL = "SELECT * FROM room WHERE BUILDINGID LIKE '%".$building."%' OR ROOMID LIKE '%".$room."%'";
+    $result = mysqli_query($link, $SQL);
+    while($row = mysqli_fetch_assoc($result)) {
+        echo json_encode($row);
+    }
+}
+?>
