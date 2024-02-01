e6ac77305a81ed62c59ccf4c6b38356d8918ffca
tsaiyichen <easonjessiejj@gmail.com>
2024-02-01 23:32:03 +0800

    2024/02/01 search_library.php

--- search_library.php
+++ search_library.php
@@ -3,7 +3,7 @@
 $keyword = $_POST['keyword'];
 $link = @mysqli_connect("localhost", "root", "12345678", "lazyguide");
 
-$librarySQL = "SELECT * FROM library WHERE LIBRARYNAME LIKE '%" . $keyword . "%' OR LIBRARYDESCRIPTION LIKE '%" . $keyword . "%'";
+$librarySQL = "SELECT * FROM library WHERE LIBRARYNAME LIKE '%" . $keyword . "%' OR LIBRARYDESCRIPTION LIKE '%".$keyword."%'";
 $libraryResult = mysqli_query($link, $librarySQL);
 
 while ($row = mysqli_fetch_assoc($libraryResult)) {
