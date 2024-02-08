aecf7415fb5a1a720582d9bf8c05677963f6e66c
tsaiyichen <easonjessiejj@gmail.com>
2024-02-08 19:26:29 +0800

    2024/02/08 mail.php

--- mail.php
+++ mail.php
@@ -4,11 +4,7 @@ $subject = "Verification Code";
 $verification_code = rand(100000, 999999);
 $message = "Your verification code is: " . $verification_code;
 
-$headers = "From: project.lazyguide@gmail.com\r\n";
-$headers .= "Reply-To: project.lazyguide@gmail.com\r\n";
-$headers .= "Content-type: text/html\r\n";
-
-mail($email, $subject, $message, $headers);
+mail($email, $subject, $message);
 
 echo (string)$verification_code;
 ?>
