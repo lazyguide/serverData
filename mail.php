b0d7629849638bf98461db7bc9b9e653668180de
tsaiyichen <easonjessiejj@gmail.com>
2024-02-08 22:46:49 +0800

    2024/02/08 use outlook SMTP

--- mail.php
+++ mail.php
@@ -11,15 +11,15 @@ $mail = new PHPMailer(true);
 $mail->isSMTP();                                            //Send using SMTP
 $mail->Host       = 'smtp-mail.outlook.com';                     //Set the SMTP server to send through
 $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-$mail->Username   = 'project.lazyguide@gmail.com';                     //SMTP username
+$mail->Username   = 'project.lazyguide@outlook.com';                     //SMTP username
 $mail->Password   = 'IM41project@lazyguide';                               //SMTP password
 $mail->SMTPSecure = 'tls';            //Enable implicit TLS encryption
 $mail->Port       = 587;
 
-$mail->setFrom('project.lazyguide@gmail.com', 'lazyguide');
+$mail->setFrom('project.lazyguide@outlook.com', 'lazy guide');
 $mail->addAddress($email);
 
-$mail->subject = "lazyguide Verification";
+$mail->Subject = "lazyguide Verification";
 $mail->Body = "Verification Code: ".$verificationCode;
 
 $mail->send();
