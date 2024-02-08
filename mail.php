35769214bb32ccea50c3e7af2925022bc78f20ae
tsaiyichen <easonjessiejj@gmail.com>
2024-02-08 21:01:41 +0800

    2024/02/08 mail.php use SMTP

--- mail.php
+++ mail.php
@@ -1,10 +1,38 @@
 <?php
-$email = $_POST['email'];
-$subject = "Verification Code";
+
+$smtpServer = 'smtp.gmail.com';
+$smtpPort = 587;
+$username = 'project.lazyguide@gmail.com';
+$password = 'IM41project@lazyguide';
+
+$recipient = $_POST['email'];
+$subject = 'Verification';
 $verification_code = rand(100000, 999999);
 $message = "Your verification code is: " . $verification_code;
 
-mail($email, $subject, $message);
+$headers = array(
+    'From' => $username,
+    'To' => $recipient,
+    'Subject' => $subject,
+    'Reply-To' => $username,
+    'MIME-Version' => '1.0',
+    'Content-Type' => 'text/html; charset=utf-8'
+);
 
-echo (string)$verification_code;
+$smtp = @Mail::factory('smtp', array(
+    'host' => $smtpServer,
+    'port' => $smtpPort,
+    'auth' => true,
+    'username' => $username,
+    'password' => $password
+));
+
+$mail = @$smtp->send($recipient, $headers, $message);
+if (PEAR::isError($mail)) {
+    echo 'Error: ' . $mail->getMessage();
+} else {
+    echo (string)$verification_code;
+}
+
+
 ?>
