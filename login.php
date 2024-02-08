6e1d439ec21fdb5f44a548329f12b0b421a0d7dc
tsaiyichen <easonjessiejj@gmail.com>
2024-02-08 18:04:49 +0800

    2024/02/08 login.php

--- login.php
+++ login.php
@@ -1,19 +1,20 @@
 <?php
-$username = $_POST["username"];
+$userID = $_POST["userID"];
 $password = $_POST["password"];
 
 $link = @mysqli_connect("localhost", "root", "12345678", "account");
-$sql = "SELECT username, password FROM user WHERE username = '$username' AND password = '$password'";
+$sql = "SELECT * FROM user WHERE username = '$userID' AND password = '$password'";
 
 $result = mysqli_query($link, $sql);
 
 if($row = mysqli_fetch_assoc($result)){
-    $array = array("status" => "success", "username" => $username);
+    $array = array("status" => "success", "userID" => $userID, "userName" => $row['USERNAME'],
+                    "password" => $password, "phone" => $row["phone"], "isVarified" => $row["ISVARIFIED"], "level" => $row["level"]);
     echo json_encode($array);
-
 }
 else{
-    $array = array("status" => "fail", "username" => $username);
+    $array = array("status" => "fail", "userID" => $userID, "userName" => NULL,
+        "password" => $password, "phone" => NULL, "isVarified" => NULL, "level" => NULL);
     echo json_encode($array);
 }
 ?>
