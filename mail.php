5c93061b5b565e03bcdcb6be2bc747f536d82c6f
tsaiyichen <easonjessiejj@gmail.com>
2024-02-08 21:14:33 +0800

    2024/02/08 mail.php back

--- mail.php
+++ mail.php
@@ -1,38 +1,8 @@
 <?php
+    $to = $_POST['email'];
+    $varificationCode = rand(100000, 999999);
+    $subject = "Verification Code: ";
+    $message = "Your code: ".$varificationCode;
 
-$smtpServer = 'smtp.gmail.com';
-$smtpPort = 587;
-$username = 'project.lazyguide@gmail.com';
-$password = 'IM41project@lazyguide';
-
-$recipient = $_POST['email'];
-$subject = 'Verification';
-$verification_code = rand(100000, 999999);
-$message = "Your verification code is: " . $verification_code;
-
-$headers = array(
-    'From' => $username,
-    'To' => $recipient,
-    'Subject' => $subject,
-    'Reply-To' => $username,
-    'MIME-Version' => '1.0',
-    'Content-Type' => 'text/html; charset=utf-8'
-);
-
-$smtp = @Mail::factory('smtp', array(
-    'host' => $smtpServer,
-    'port' => $smtpPort,
-    'auth' => true,
-    'username' => $username,
-    'password' => $password
-));
-
-$mail = @$smtp->send($recipient, $headers, $message);
-if (PEAR::isError($mail)) {
-    echo 'Error: ' . $mail->getMessage();
-} else {
-    echo (string)$verification_code;
-}
-
-
+    mail($to, $subject, $message);
 ?>
