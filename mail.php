d80b74989bb3ecc05256cce7ad3e37dffd70756c
tsaiyichen <easonjessiejj@gmail.com>
2024-02-08 21:17:32 +0800

    2024/02/08 mail.php SMTP + port

--- mail.php
+++ mail.php
@@ -1,4 +1,7 @@
 <?php
+    ini_set("SMTP", "smtp.gmail.com");
+    ini_set("smtp_port", 587);
+
     $to = $_POST['email'];
     $varificationCode = rand(100000, 999999);
     $subject = "Verification Code: ";
