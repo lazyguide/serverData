9ec25a993d38813b2fe207d91bc9b6dccb23ed66
tsaiyichen <easonjessiejj@gmail.com>
2024-02-08 21:24:16 +0800

    2024/02/08 mail.php +header

--- mail.php
+++ mail.php
@@ -6,8 +6,11 @@
     $varificationCode = rand(100000, 999999);
     $subject = "Verification Code: ";
     $message = "Your code: ".$varificationCode;
+    $headers = "From: project.lazyguide@gmail.com\r\n";
+    $headers .= "Reply-To: project.lazyguide@gmail.com\r\n";
+    $headers .= "Return-Path: project.lazyguide@gmail.com\r\n";
 
-    if (mail($to, $subject, $message)) {
+    if (mail($to, $subject, $message, $headers)) {
         echo "Email sent successfully!";
     } else {
         echo "Failed to send email.";
