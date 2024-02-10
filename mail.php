1a51f2a11f39e30d0272a58f3dc75aaa6428a095
lazyGuideServer <project.lazyguide@gmail.com>
2024-02-10 23:28:38 +0800

    2024/02/10 error message

--- mail.php
+++ mail.php
@@ -7,22 +7,25 @@ require 'vendor/autoload.php';
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
-$mail = new PHPMailer(true);
-$mail->isSMTP();                                            //Send using SMTP
-$mail->Host       = 'smtp-mail.outlook.com';                     //Set the SMTP server to send through
-$mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-$mail->Username   = 'project.lazyguide@outlook.com';                     //SMTP username
-$mail->Password   = 'IM41project@lazyguide';                               //SMTP password
-$mail->SMTPSecure = 'tls';            //Enable implicit TLS encryption
-$mail->Port       = 587;
+try {
+    $mail = new PHPMailer(true);
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host = 'smtp-mail.outlook.com';                     //Set the SMTP server to send through
+    $mail->SMTPAuth = true;                                   //Enable SMTP authentication
+    $mail->Username = 'project.lazyguide@outlook.com';                     //SMTP username
+    $mail->Password = 'IM41project@lazyguide';                               //SMTP password
+    $mail->SMTPSecure = 'tls';            //Enable implicit TLS encryption
+    $mail->Port = 587;
 
-$mail->setFrom('project.lazyguide@outlook.com', 'lazy guide');
-$mail->addAddress($email);
+    $mail->setFrom('project.lazyguide@outlook.com', 'lazy guide');
+    $mail->addAddress($email);
 
-$mail->Subject = "lazyguide Verification";
-$mail->Body = "Verification Code: ".$verificationCode;
+    $mail->Subject = "lazyguide Verification";
+    $mail->Body = "Verification Code: " . $verificationCode;
 
-$mail->send();
-
-echo (string)$verificationCode;
+    $mail->send();
+    echo (string)$verificationCode;
+} catch(Exception $e){
+    echo "Error: {$mail->ErrorInfo}";
+}
 ?>
