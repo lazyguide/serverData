2e18345edc1eb4df0492d72dcb10dee7900caa86
lazyGuideServer <project.lazyguide@gmail.com>
2024-02-29 03:16:12 +0800

    2024/02/29 test echo

--- getFacilityData.php
+++ getFacilityData.php
@@ -6,8 +6,8 @@ $length = strlen($str);
 
 $firstPart = substr($str, 0, $length - 2); // Extract all but the last two characters
 $lastTwoChars = substr($str, -2); // Extract the last two characters
-echo $firstPart, $lastTwoChars;
 $ID = (int)($lastTwoChars);
+echo $firstPart, $ID;
 
 $sql = "SELECT * FROM facility WHERE FACILIYTTYPE = '$firstPart' AND FACILITYID = $ID";
 
