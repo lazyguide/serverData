2a8061de9449d5c49bf158031f95bb3332f56241
tsaiyichen <easonjessiejj@gmail.com>
2024-02-29 01:43:34 +0800

    2024/02/29 getFacilityData.php done

---
+++ getFacilityData.php
@@ -0,0 +1,18 @@
+<?php
+include("config.php");
+
+$str = $_POST['facilityName'];
+$length = strlen($str);
+
+$firstPart = substr($str, 0, $length - 2); // Extract all but the last two characters
+$lastTwoChars = substr($str, -2); // Extract the last two characters
+
+$ID = (int)($lastTwoChars);
+
+$sql = "SELECT * FROM facility WHERE FACILIYTTYPE = '$firstPart' AND FACILITYID = $ID";
+
+$result = mysqli_query($link, $sql);
+
+$row = mysqli_fetch_assoc($result);
+echo json_encode($row);
+?>
