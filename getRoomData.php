8d7d8ec0239a7e6103a0c1edbe65291a12ed5b05
lazyGuideServer <project.lazyguide@gmail.com>
2024-03-01 22:15:12 +0800

    buildingID

--- getRoomData.php
+++ getRoomData.php
@@ -2,7 +2,7 @@
 $ID = $_POST['buildingID'];
 $floor = $_POST['floor'];
 $link = @mysqli_connect('localhost', 'root', '12345678', 'lazyguide');
-$SQL = "SELECT * FROM room WHERE BULIDINGID = '$ID' AND FLOOR = $floor";
+$SQL = "SELECT * FROM room WHERE BUILDINGID = '$ID' AND FLOOR = $floor";
 
 $result = mysqli_query($link, $SQL);
 
