da5d63995b9fa93600e59ee9888835dd36ae936c
tsaiyichen <easonjessiejj@gmail.com>
2024-03-01 16:28:23 +0800

    2024/03/01 getBuildingData.php

---
+++ getBuildingData.php
@@ -0,0 +1,14 @@
+<?php
+$ID = $_POST['buildingID'];
+$link = @mysqli_connect('localhost', 'root', '12345678', 'lazyguide');
+$SQL = "SELECT * FROM building WHERE BULIDINGID = '$ID'";
+
+$result = mysqli_query($link, $SQL);
+
+if($result){
+    $row = mysqli_fetch_assoc($result);
+    echo json_encode($row);
+}else{
+    echo "error";
+}
+?>
