82e770011595e7ba23cab161b04f4a1ccc516b82
tsaiyichen <easonjessiejj@gmail.com>
2024-03-02 22:49:04 +0800

    2024/03/02 getRoomData.php

--- getRoomData.php
+++ getRoomData.php
@@ -2,11 +2,15 @@
 $ID = $_POST['buildingID'];
 $floor = $_POST['floor'];
 $link = @mysqli_connect('localhost', 'root', '12345678', 'lazyguide');
-$SQL = "SELECT * FROM room WHERE BUILDINGID = '$ID' AND FLOOR = $floor";
+$SQL = "SELECT * FROM room WHERE BULIDINGID = '$ID' AND FLOOR = $floor";
 
 $result = mysqli_query($link, $SQL);
 
 while($row = mysqli_fetch_assoc($result)){
-    echo json_encode($row);
+    if(strpos($ID, "/")){
+        $row['BUILDINGID'] = substr($row['ROOMID'], 0, 2);
+        $row['ROOMID'] = substr($row['ROOMID'], 2);
+    }
+    json_encode($row);
 }
 ?>
