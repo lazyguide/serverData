dd4a2aeef602a2c5fb16c708381f6502cce719cc
tsaiyichen <easonjessiejj@gmail.com>
2024-03-07 14:32:41 +0800

    2024/03/07 modify SQL statement

--- search_room.php
+++ search_room.php
@@ -9,9 +9,9 @@ while($roomRow = mysqli_fetch_assoc($roomResult)) {
     echo json_encode($roomRow);
 }
 
-if(strlen($keyword) > 3){
+if(strlen($keyword) >= 3){
     $building = substr($keyword,0,2);
-    $room = substr($keyword, 2, 3);
+    $room = substr($keyword, 2, strlen($keyword) - 2);
     $SQL = "SELECT * FROM room WHERE BUILDINGID LIKE '%".$building."%' AND ROOMID LIKE '%".$room."%'";
     $result = mysqli_query($link, $SQL);
     while($row = mysqli_fetch_assoc($result)) {
