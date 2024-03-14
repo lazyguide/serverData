718677e536d2966ed66ff5fdda2ae403bbb30838
YYT0313 <yungting111@gmail.com>
2024-03-14 18:09:46 +0800

    search_IDRactivity.php TURE => 1

--- search_IDRactivity.php
+++ search_IDRactivity.php
@@ -2,7 +2,7 @@
 $keyword = $_POST['keyword'];
 
 $link = @mysqli_connect('localhost', 'root', '12345678', 'lazyguide');
-$sql = "SELECT * FROM indoor_activity WHERE ISVARIFIED = TRUE AND (IDR_ACTNAME LIKE '%".$keyword."%' OR
+$sql = "SELECT * FROM indoor_activity WHERE ISVARIFIED = 1 AND (IDR_ACTNAME LIKE '%".$keyword."%' OR
                 IDR_ACT_DESCRIPTION LIKE '%".$keyword."%' OR TYPE LIKE '%".$keyword."%' OR ROOMID LIKE
                     '%".$keyword."%' OR BUILDINGID LIKE '%".$keyword."%'";
 $result = mysqli_query($link, $sql);
