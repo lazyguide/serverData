d5619d11a6aac8631537507f642740b0539cdc84
YYT0313 <yungting111@gmail.com>
2024-03-14 18:24:06 +0800

    search_ODRactivity.php

---
+++ search_ODRactivity.php
@@ -0,0 +1,16 @@
+<?php
+$keyword = $_POST['keyword'];
+
+$link = @mysqli_connect('localhost', 'root', '12345678', 'lazyguide');
+$sql = "SELECT * FROM outdoor_activity odr
+        WHERE ISVARIFIED = 1 AND odr.PLACEID = place.PLACEID
+        AND (ODR_ACTNAME LIKE '%".$keyword."%' 
+        OR ODR_ACT_DESCRIPTION LIKE '%".$keyword."%' 
+        OR TYPE LIKE '%".$keyword."%' 
+        OR PLACENAME LIKE '%".$keyword."%'";
+
+$result = mysqli_query($link, $sql);
+while($row = mysqli_fetch_assoc($result)){
+    echo json_encode($row);
+}
+?>
