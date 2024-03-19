ceba7e2e34fa02ed0335a1d2fa35580c45c81578
tsaiyichen <easonjessiejj@gmail.com>
2024-03-20 00:37:13 +0800

    2024/03/20

--- settingUpdate.php
+++ settingUpdate.php
@@ -4,7 +4,7 @@ $newUsername = $_POST['username'];
 $newPassword = $_POST['password'];
 $newPhone = $_POST['phone'];
 
-$link = @mysqli_connect('localhost','root','','lazyguide');
+$link = @mysqli_connect('localhost','root','12345678','lazyguide');
 
 $sql = "UPDATE account SET username='$newUsername', password='$newPassword', phone='$newPhone' 
         WHERE userid='$userid'";
