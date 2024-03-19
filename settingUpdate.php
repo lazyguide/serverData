295bc06f2b75e64b7365d8c1b6f6adcb35da5ec9
tsaiyichen <easonjessiejj@gmail.com>
2024-03-20 00:40:24 +0800

    2024/03/20 result message

--- settingUpdate.php
+++ settingUpdate.php
@@ -6,9 +6,13 @@ $newPhone = $_POST['phone'];
 
 $link = @mysqli_connect('localhost','root','12345678','lazyguide');
 
-$sql = "UPDATE account SET username='$newUsername', password='$newPassword', phone='$newPhone' 
-        WHERE userid='$userid'";
+$sql = "UPDATE account SET username = '$newUsername', password = '$newPassword', phone = '$newPhone' WHERE userid = '$userid'";
 $result = mysqli_query($link, $sql);
 
+if($result){
+    echo "success";
+}else{
+    echo "fail";
+}
 
 ?>
