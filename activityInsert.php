817f7e27c82ca19f91ed36a24f346a282d29b80c
tsaiyichen <easonjessiejj@gmail.com>
2024-03-21 01:42:07 +0800

    2024/03/21 activityInsert.php

---
+++ activityInsert.php
@@ -0,0 +1,42 @@
+<?php
+$link = @mysqli_connect('localhost', 'root', '12345678', 'lazyguide');
+
+if ($link->connect_error) {
+    die("Connection failed: " . $link->connect_error);
+}
+
+$dropdownSelection = $_POST['dropdownSelection'];
+$userInput = $_POST['userInput'];
+$InputDetail = $_POST['InputDetail'];
+$SdateTime = $_POST['dateTimeString'];
+$EdateTime = $_POST['edateTimeString'];
+$type = $_POST['type'];
+$method = $_POST['method'];
+$place = $_POST['place'];
+
+$userid = $_POST['userID'];
+$buildingId = $_POST['building'];
+$roomId = $_POST['room'];
+$placeID = $_POST['placeID'];
+
+if ($dropdownSelection == "室內")
+{
+    $sql = "INSERT INTO indoor_activity (IDR_ACTNAME, IDR_ACT_DESCRIPTION, IDR_STARTDATE, IDR_ENDDATE, TYPE, METHOD, ROOMID, BUILDINGID, USERID) VALUES ('$userInput', '$InputDetail', '$SdateTime', '$EdateTime', '$type', '$method', '$roomId', '$buildingId', '$userid')";
+}
+elseif ($dropdownSelection == "室外")
+{
+    $sql = "INSERT INTO outdoor_activity (ODR_ACTNAME, ODR_ACT_DESCRIPTION, ODR_ACT_STARTDATE, ODR_ENDDATE, TYPE, METHOD, PLACEID, USERID) VALUES ('$userInput', '$InputDetail', '$SdateTime', '$EdateTime', '$type', '$method', $placeID, '$userid')";
+}
+else
+{
+    die("Invalid dropdown selection");
+}
+
+if ($link->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $link->error;
+}
+
+$link->close();
+?>
