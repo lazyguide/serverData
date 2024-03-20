a37773ad22f3061e410779865a14a21b10965fea
tsaiyichen <easonjessiejj@gmail.com>
2024-03-21 00:45:15 +0800

    2024/03/21 activityRoom.php

---
+++ activityRoom.php
@@ -0,0 +1,14 @@
+<?php
+$building = $_POST['building'];
+$floor = $_POST['floor'];
+$link = mysqli_connect('localhost', 'root', '12345678', 'lazyguide');
+
+$sql = "SELECT * FROM room WHERE BUILDINGID = '$building' AND FLOOR = $floor";
+
+$ans = [];
+$result = mysqli_query($link, $sql);
+while($row = mysqli_fetch_assoc($result)){
+    array_push($ans, $row['ROOMID']);
+}
+echo json_encode($ans);
+?>
