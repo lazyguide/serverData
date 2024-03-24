8449953a1f607b777b742c906c56cb307e1d5688
tsaiyichen <easonjessiejj@gmail.com>
2024-03-25 00:53:32 +0800

    2024/03/24 changeISVERIFIED.php

--- changeISVERIFIED.php
+++ changeISVERIFIED.php
@@ -5,7 +5,7 @@ $link = @mysqli_connect('localhost', 'root', '12345678', 'lazyguide');
 if($table = "indoor_activity"){
     $sql = "UPDATE indoor_activity SET VCODEVERIFY = 1 WHERE IDR_ACTID = $actID";
 }else{
-    $sql = "UPDATE indoor_activity SET VCODEVERIFY = 1 WHERE ODR_ACTID = $actID";
+    $sql = "UPDATE outdoor_activity SET VCODEVERIFY = 1 WHERE ODR_ACTID = $actID";
 }
 $result = mysqli_query($link, $sql);
 
