1888552e1120f66f79d71ec535af2502df0d08a9
tsaiyichen <easonjessiejj@gmail.com>
2024-03-27 01:22:11 +0800

    2024/03/27 cal $row

--- cal_outdoorData.php
+++ cal_outdoorData.php
@@ -44,7 +44,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         // 檢查查詢結果
         if ($result->num_rows > 0) {
             while($row = mysqli_fetch_assoc($result)) {
-                echo json_encode($result);
+                echo json_encode($row);
             }
         } else {
             // 輸出一個空 JSON 對象
